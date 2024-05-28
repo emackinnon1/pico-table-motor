0f354f8617edeee700b8c5204741bb59a58b0337
Elliot <49004020+emackinnon1@users.noreply.github.com>
2024-05-28 16:39:59 -0600

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,42 +3,35 @@ from machine import Pin
 import network
 from umqtt.simple import MQTTClient
 
-# consts
 led = machine.Pin("LED", machine.Pin.OUT)
-in1 = machine.Pin(0, machine.Pin.OUT)
-in2 = machine.Pin(1, machine.Pin.OUT)
-button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
-
-# global vars
-wlan_cnnct_attempts = 0
-table_state = ""
-offline = False
+in1 = machine.Pin(16, machine.Pin.OUT)
+in2 = machine.Pin(17, machine.Pin.OUT)
+in3 = machine.Pin(14, machine.Pin.OUT)
+in4 = machine.Pin(15, machine.Pin.OUT)
 
 # connect to wifi
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect("{NETWORK_NAME}", "{PASSWORD}")
-
+wlan.connect("Who pooped in the pool", "Sonoffinan1!")
 while not wlan.isconnected():
     print("Connecting...")
-    time.sleep(.5)
-    wlan_cnnct_attempts = wlan_cnnct_attempts + 1
-    if wlan_cnnct_attempts >= 3:
-        offline = True
-        
+    time.sleep(1)
 print("Connected!")
 # mqtt vars
-mqtt_server = '{MQTT_SERVER}'
-mqtt_user = '{MQTT_USER}'
-mqtt_password = '{MQTT_PASSWORD}'
+mqtt_server = '10.0.0.91'
+mqtt_user = 'mqtt_user'
+mqtt_password = 'Sonoffinan'
 client_id = 'table_motor_switch'
 sub_topic = 'cmd/table_motor_switch'
 pub_topic = 'state/table_motor_switch'
+mqtt_status_topic = 'state/table_motor_switch/status'
 PING_INTERVAL = 60
 mqtt_con_flag = False
 pingresp_rcv_flag = True
 lock = True
 next_ping_time = 0
+
+table_state = ""
 
 # connect to mqtt
 mqtt_client = MQTTClient(
@@ -53,9 +46,16 @@ def drive_motor(state):
     if state == 'OPEN':
         in1.high()
         in2.low()
+        in3.high()
+        in4.low()
     if state == 'CLOSED':
         in1.low()
         in2.high()
+        in3.low()
+        in4.high()
+    for i in range(6):
+        led.toggle()
+        time.sleep(0.1)
 
 def mqtt_subscription_callback(topic, message):
     global table_state
@@ -65,12 +65,14 @@ def mqtt_subscription_callback(topic, message):
     if table_state == 'OPEN':
         print("opening table")
         table_state = msg
+        drive_motor(table_state)
     if table_state == 'CLOSED':
         print("closing table")
         table_state = msg
-    drive_motor(table_state)
-    mqtt_client.publish(pub_topic, table_state)
+        drive_motor(table_state)
+    mqtt_client.publish(pub_topic, table_state, retain=True)
 
+mqtt_client.set_last_will(mqtt_status_topic, "disconnected", retain=True)
 mqtt_client.set_callback(mqtt_subscription_callback)
 
 def mqtt_connect():
@@ -89,6 +91,7 @@ def mqtt_connect():
             pingresp_rcv_flag = True
             next_ping_time = time.time() + PING_INTERVAL
             lock = False
+            mqtt_client.publish(mqtt_status_topic, "connected", retain=True)
         except Exception as e:
             print("Error in mqtt connect: [Exception]  %s: %s" % (type(e).__name__, e))
         time.sleep(0.5)
@@ -112,33 +115,14 @@ def check():
     if (time.time() >= next_ping_time):
         ping()
     mqtt_client.check_msg()
-    check_button_state(button.value())
-    
-def check_button_state(button_pressed):
-    global table_state
-    global offline
-    
-    if button_pressed:
-        table_state = "OPEN"
-        print("opening table")
-    elif button_pressed:
-        table_state = "CLOSED"
-    drive_motor(table_state)
-    if not offline:
-        mqtt_client.publish(pub_topic, table_state)
-    
     
 while True:
-    if not offline:
-        try:
-            mqtt_connect()
-            check()
-        except Exception as e:
-            print("Error: [Exception] %s: %s" % (type(e).__name__, e))
-            lock = True
-            mqtt_con_flag = False
-            raise e
-    else:
-        check_button_state(button.value())
-        
+    mqtt_connect()
+    try:
+        check()
+    except Exception as e:
+        print("Error: [Exception] %s: %s" % (type(e).__name__, e))
+        lock = True
+        mqtt_con_flag = False
+        raise e
     time.sleep(.1)
